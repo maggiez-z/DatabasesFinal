c80618fa9b1bbee2b56db32a031b9c1c0866e072
Matt <mae108@case.edu>
2021-11-23 17:15:32 -0500

    updated routing and auth stuff

--- SpartanDiningGuideFrontend/src/app/app-routing.module.ts
+++ SpartanDiningGuideFrontend/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuardGuard } from './auth-guard.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login' 
+  },
+  { 
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+    canActivate: [AuthGuardGuard],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ SpartanDiningGuideFrontend/src/app/auth-guard.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from './services/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardGuard implements CanActivate {
+
+  constructor(
+    public ls: LoginService
+  ) {
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.ls.loggedIn;
+  }
+  
+}
